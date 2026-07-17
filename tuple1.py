9381d688bc7e99675f652aefae5b56a0efb78a62
payaldevgade <payaldevgade13@gmail.com>
2026-07-17 20:48:22 +0530

    Add files via upload

---
+++ tuple1.py
@@ -0,0 +1,7 @@
+a = (12,23,49,5.67,print(),"hello")
+
+for i in range(len(a)):
+    print(i)
+
+for i in a:
+    print(i)    
---
+++ tuplemethods.py
@@ -0,0 +1,13 @@
+a = (1,2,3,3,3,3,3,3,4,5,print(),"hello")
+
+# index method
+
+index = a.index(4)
+
+print(index)
+
+# count method 
+
+count = a.count(3)
+
+print(count)
